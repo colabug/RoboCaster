ebe5bfc66ccb3084ae64df0632e657872b81ec31
Corey Leigh Latislaw <colabug@gmail.com>
2016-03-24 09:43:31 -0400

    Fixed typo

--- app/src/test/java/com/greenlifesoftware/robolectricpractice/RobolectricActivityTest.java
+++ app/src/test/java/com/greenlifesoftware/robolectricpractice/RobolectricActivityTest.java
@@ -1,6 +1,7 @@
 package com.greenlifesoftware.robolectricpractice;
 
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
@@ -11,8 +12,8 @@ import static org.junit.Assert.assertNotNull;
 @Config(constants = BuildConfig.class, sdk = 21)
 public class RobolectricActivityTest
 {
-    @Before
-    public void setUp() throws Exception
+    @Test
+    public void shouldNotBeNull() throws Exception
     {
         RobolectricActivity activity = Robolectric.setupActivity( RobolectricActivity.class );
         assertNotNull( activity );
