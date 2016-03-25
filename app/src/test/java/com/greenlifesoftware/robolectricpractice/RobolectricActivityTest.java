c5f2298335cd13fee014dc80f44506006bfdc30d
Corey Leigh Latislaw <colabug@gmail.com>
2016-03-25 17:39:30 -0400

    Test 1: Get title

--- app/src/test/java/com/greenlifesoftware/robolectricpractice/RobolectricActivityTest.java
+++ app/src/test/java/com/greenlifesoftware/robolectricpractice/RobolectricActivityTest.java
@@ -5,8 +5,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
+
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
 
 @RunWith(RobolectricGradleTestRunner.class)
 @Config(constants = BuildConfig.class, sdk = 21)
@@ -24,5 +28,12 @@ public class RobolectricActivityTest
     public void shouldNotBeNull() throws Exception
     {
         assertNotNull( activity );
+    }
+
+    @Test
+    public void shouldHaveTitle() throws Exception
+    {
+        assertThat( activity.getTitle().toString(),
+                    equalTo( RuntimeEnvironment.application.getString( R.string.app_name ) ) );
     }
 }
